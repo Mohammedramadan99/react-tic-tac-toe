0925bc9a3a6eadea745fec84c6ff2c399ae2239a
Alex Erdei <mralexerdei@yahoo.co.uk>
2022-01-29 08:35:34 +0000

    add gameboard.js

---
+++ src/gameboard.js
@@ -0,0 +1,30 @@
+import { matrix } from "./matrix";
+
+export const gameBoard = (function () {
+  const _n = 3;
+
+  let _board = matrix(_n, _n);
+
+  function setField(col, row, char) {
+    _board.setElement(col, row, char);
+  }
+
+  function getField(col, row) {
+    return _board.getElement(col, row);
+  }
+
+  function erase() {
+    _board.fillUp("");
+  }
+
+  function getN() {
+    return _n;
+  }
+
+  function getBoard() {
+    return _board.getMatrix();
+  }
+
+  erase();
+  return { getN, setField, getField, erase, getBoard };
+})();
