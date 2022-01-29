4ad7775dd9f2308eed9df32b167baed29c2ba1be
Alex Erdei <mralexerdei@yahoo.co.uk>
2022-01-29 10:31:02 +0000

    render board

--- src/App.js
+++ src/App.js
@@ -1,23 +1,32 @@
-import logo from "./logo.svg";
+import React, { useState } from "react";
 import "./App.css";
+import { gameBoard } from "./gameboard";
 
 function App() {
+  const N = gameBoard.getN();
+
+  const [state, setState] = useState({
+    board: gameBoard,
+  });
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="board">
+        {state.board.getBoard().map((column, col) => {
+          column.map((cell, row) => {
+            return (
+              <div
+                key={`(${row}, ${col})`}
+                row={row}
+                col={col}
+                className="cell"
+              >
+                `(${row}, ${col})`
+              </div>
+            );
+          });
+        })}
+      </div>
     </div>
   );
 }
